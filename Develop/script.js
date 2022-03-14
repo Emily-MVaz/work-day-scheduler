ed55feac48c61fa3c2d57bf2cc40f3bf33a5a83d
Emily Vazquez <emily.m.vazquez99@gmail.com>
2022-03-13 23:09:22 -0500

    Initial Commit added first few time block rows and columns styled and added save button added text area to center began working on local storage

---
+++ Develop/script.js
@@ -0,0 +1,36 @@
+$( document ).ready(function () {
+
+//local storage grab
+$("#09 .textbox").val(localStorage.getItem("09"));
+$("#10 .textbox").val(localStorage.getItem("10"));
+$("#11 .textbox").val(localStorage.getItem("11"));
+$("#12 .textbox").val(localStorage.getItem("12"));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+})
