facdd492fac354d504c2589821b3c6913cf5bc78
Emily Vazquez <emily.m.vazquez99@gmail.com>
2022-03-14 18:15:03 -0500

    finished javascript file took out and fixed styling tags I added edited html and js to match original css added moment js info to javascript file corrected local storage save

--- Develop/script.js
+++ Develop/script.js
@@ -1,17 +1,70 @@
 $( document ).ready(function () {
-
-//local storage grab
-$("#09 .textbox").val(localStorage.getItem("09"));
-$("#10 .textbox").val(localStorage.getItem("10"));
-$("#11 .textbox").val(localStorage.getItem("11"));
-$("#12 .textbox").val(localStorage.getItem("12"));
+    //setting current day at top
+    $("#currentDay").text(moment().format('MMMM Do YYYY, h:mm a'));
 
 
+   //event listener for save button
+   $(".saveBtn").click(function() {
+        
+        //grab values from description
+        // console.log(this);
+        var text = $(this).siblings(".description").val();
+        var time = $(this).parent().attr("id")
+
+        //put in local storage
+        localStorage.setItem(time, text);
+    })
+
+    //local storage grab
+    $("#09 .description").val(localStorage.getItem("09"));
+    $("#10 .description").val(localStorage.getItem("10"));
+    $("#11 .description").val(localStorage.getItem("11"));
+    $("#12 .description").val(localStorage.getItem("12"));
+    $("#13 .description").val(localStorage.getItem("13"));
+    $("#14 .description").val(localStorage.getItem("14"));
+    $("#15 .description").val(localStorage.getItem("15"));
+    $("#16 .description").val(localStorage.getItem("16"));
+    $("#17 .description").val(localStorage.getItem("17"));
 
 
 
+    function timeTracker() {
+        var currentHour = moment().hour();
+
+        //looping over each hour block
+        $(".time-block").each(function () {
+            var hourBlock =  parseInt($(this).attr("id"));
+            // console.log(hourBlock, currentHour)
+
+            //when the hourblock is before the current time
+            if(hourBlock < currentHour) {
+                //classes found in css file change hour block color
+                $(this).addClass("past");
+                // $(this).removeClass("present");
+                // $(this).removeClass("future");
+            }
+            //when the hourblock is the same as the current time
+            else if (hourBlock === currentHour) {
+                $(this).removeClass("past");
+                $(this).addClass("present");
+                // $(this).removeClass("future");
+            }
+            //when the hourblock is after the current time  
+            else { 
+                $(this).removeClass("past");
+                $(this).removeClass("present");
+                $(this).addClass("future");
+            }
+        })
+
+    }
+
+    //run function
+    timeTracker();
 
 
+
+ 
 
 
 
